9fe6b8f5119b7c47b6429cd7243d1d813ca7be36
gautier kiss <gkgkgk395@gmail.com>
2021-08-31 18:05:22 +0200

    multiplication and substraction

--- utils/binary_operations.c
+++ utils/binary_operations.c
@@ -25,6 +25,21 @@ uint64_t small_to_big_endian_64(uint64_t n)
     return (n);
 }
 
+static uint8_t     *discard_useless_bytes(uint8_t *nb, uint32_t size, uint32_t *new_size)
+{
+    uint32_t c = 0;
+
+    for (uint32_t i = 0; i < size; ++i) {
+        if(nb[i] != 0)
+            break;
+        ++c;
+    }
+    *new_size = size -c;
+    uint8_t *res = calloc(size - c, 1);
+    memcpy(res, &nb[c], size -c);
+    return res;
+}
+
 static bool is_zero(uint8_t *nb1, uint32_t size){
     for(uint32_t i = 0; i < size; ++i){
         if(nb1[i] != 0)
@@ -33,7 +48,31 @@ static bool is_zero(uint8_t *nb1, uint32_t size){
     return true;
 }
 
-uint8_t     *addition(uint8_t *nb1, uint8_t *nb2, uint32_t size)
+void shift_left(uint8_t *number, uint32_t size, uint32_t bit) {
+    uint8_t rest = 0;
+    uint8_t cpy[size];
+    memcpy(cpy, number, size);
+
+    for(int i = (int)((size) - 1); i >= 0; --i){
+        number[i] = number[i] << bit | rest;
+        rest = 0;
+        rest |= cpy[i] >> (8 - bit);
+    }
+}
+
+void shift_right(uint8_t *number, uint32_t size, uint32_t bit) {
+    uint8_t rest = 0;
+    uint8_t cpy[size];
+    memcpy(cpy, number, size);
+
+    for(int i = 0; i < (int)size; ++i){
+        number[i] = number[i] >> bit | rest;
+        rest = 0;
+        rest |= cpy[i] << (8 - bit);
+    }
+}
+
+uint8_t     *addition(uint8_t *nb1, uint8_t *nb2, uint32_t size, uint32_t *new_size)
 {
     uint32_t double_size = size * 2;
     uint8_t x[double_size];
@@ -42,6 +81,7 @@ uint8_t     *addition(uint8_t *nb1, uint8_t *nb2, uint32_t size)
     memcpy(&x[size], nb1, size);
     memset(y, 0, double_size);
     memcpy(&y[size], nb2, size);
+
     while (is_zero(y, double_size) == false)
     {
         // carry now contains common
@@ -67,10 +107,106 @@ uint8_t     *addition(uint8_t *nb1, uint8_t *nb2, uint32_t size)
             rest |= carry[i] >> 7;
         }
     }
-    for(uint32_t i = 0; i < double_size; ++i)
-        printf("%02x ", x[i]);
-    puts("");
-    uint8_t *res = malloc(double_size);
+    uint8_t *res = calloc(double_size, 1);
     memcpy(res, x, double_size);
+    res = discard_useless_bytes(res, double_size, new_size);
+    return res;
+}
+
+uint8_t     *substraction(uint8_t *nb1, uint8_t *nb2, uint32_t size, uint32_t *new_size)
+{
+    uint8_t x[size];
+    uint8_t y[size];
+    memset(x, 0, size);
+    memcpy(x, nb1, size);
+    memset(y, 0, size);
+    memcpy(y, nb2, size);
+
+    while (is_zero(y, size) == false)
+    {
+        // carry now contains common
+        // set bits of x and y
+        uint8_t borrow[size];
+        memset(borrow, 0, size);
+        for(uint32_t i = 0; i < size; ++i){
+            borrow[i] = ~(x[i]) & y[i];
+        }
+ 
+        // Sum of bits of x and y where at
+        // least one of the bits is not set
+        for(uint32_t i = 0; i < size; ++i){
+            x[i] = x[i] ^ y[i];
+        }
+ 
+        // Carry is shifted by one so that adding
+        // it to x gives the required sum
+        uint8_t rest = 0;
+        for(int i = (int)((size) - 1); i >= 0; --i){
+            y[i] = borrow[i] << 1 | rest;
+            rest = 0;
+            rest |= borrow[i] >> 7;
+        }
+    }
+    uint8_t *res = calloc(size, 1);
+    memcpy(res, x, size);
+    res = discard_useless_bytes(res, size, new_size);
+    return res;
+}
+
+uint8_t *multiplication(uint8_t *nb1, uint8_t *nb2, uint32_t size, uint32_t *new_size)
+{
+    uint32_t tmp_size = size * 2;
+    uint8_t *a = calloc(tmp_size, 1);
+    uint8_t *b = calloc(tmp_size, 1);
+    uint8_t *res = calloc(tmp_size, 1);
+    memcpy(&a[size], nb1, size);
+    memcpy(&b[size], nb2, size);
+
+    bool first = true;
+ 
+    // While second number doesn't become 1
+    while (is_zero(b, tmp_size) == false)
+    {
+        // If second number becomes odd, add the first number to result
+        if (b[tmp_size - 1] & 1)
+        {
+            uint32_t s = 0;
+            res = addition(res, a, tmp_size, &s);
+            s = s + 1;
+            uint8_t *tmptmp = calloc(s, 1);
+            memcpy(&tmptmp[1], res, s);
+            res = tmptmp;
+
+            if(first){
+                uint8_t *tmp1 = calloc(s, 1);
+                memcpy(tmp1, &a[tmp_size - s], s);
+                free(a);
+                a = tmp1;
+
+                uint8_t *tmp2 = calloc(s, 1);
+                memcpy(tmp2, &b[tmp_size - s], s);
+                free(b);
+                b = tmp2;
+
+                first = false;
+            } else {
+                uint8_t *tmp1 = calloc(s, 1);
+                memcpy(&tmp1[s - tmp_size], a, tmp_size);
+                free(a);
+                a = tmp1;
+
+                uint8_t *tmp2 = calloc(s, 1);
+                memcpy(&tmp2[s - tmp_size], b, tmp_size);
+                free(b);
+                b = tmp2;
+            }
+            tmp_size = s;
+        }
+
+        // Double the first number and halve the second number
+        shift_left(a, tmp_size, 1);
+        shift_right(b, tmp_size, 1);
+    }
+    res = discard_useless_bytes(res, tmp_size, new_size);
     return res;
 }
